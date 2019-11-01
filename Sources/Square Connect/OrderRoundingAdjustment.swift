a9b27af4e6425a57bbc141837a84f58cd62eb86b
Justin LeBlanc <leblanc@wustl.edu>
2019-11-01 15:39:17 -0500

    Add Order and some associated files

--- Sources/Square Connect/OrderRoundingAdjustment.swift
+++ Sources/Square Connect/OrderRoundingAdjustment.swift
@@ -1,8 +1,16 @@
-//
-//  File.swift
-//  
-//
-//  Created by Justin LeBlanc on 11/1/19.
-//
-
 import Foundation
+
+/// A rounding adjustment of the money being returned
+/// Commonly used to apply Cash Rounding when the minimum unit of account is smaller than the lowest physical denomination of currency.
+public struct OrderRoundingAdjustment: Codable {
+
+    /// Actual rounding adjustment amount.
+    let amount_money: Money?
+
+    /// The name of the rounding adjustment from the original sale Order.
+    let name: String?
+
+    /// Unique ID that identifies the rounding adjustment only within this order.
+    /// **Max Length 60**
+    let uid: String?
+}
--- Sources/Square Connect/OrderSource.swift
+++ Sources/Square Connect/OrderSource.swift
@@ -1,8 +1,7 @@
-//
-//  File.swift
-//  
-//
-//  Created by Justin LeBlanc on 11/1/19.
-//
-
 import Foundation
+
+/// The origination details of the order.
+public struct OrderSource: Codable {
+    /// The name used to identify the place (physical or digital) that an order originates. If unset, the name defaults to the name of the application that created the order.
+    public let name: String?
+}
